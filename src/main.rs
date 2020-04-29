f3bbf2624bdf6e50d0c3b0eb8417722a6e913dd6
Paco van der Linden <pavadeli@gmail.com>
2020-04-29 16:08:23 +0200

    initial version

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,206 @@
-use std::path::PathBuf;
+#[macro_use]
+extern crate error_chain;
+
+use console::Style;
+use indicatif::{MultiProgress, ParallelProgressIterator, ProgressBar, ProgressStyle};
+use quick_xml::de::from_reader;
+use rayon::prelude::*;
+use std::collections::HashMap;
+use std::fs::{read_dir, File};
+use std::io::{prelude::*, BufReader};
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex};
+use std::thread;
 use structopt::StructOpt;
+
+mod manifest;
+use manifest::*;
+
+mod errors {
+    error_chain! {
+        foreign_links {
+            Io(std::io::Error);
+            Xml(quick_xml::DeError);
+        }
+    }
+}
+use errors::*;
 
 #[derive(Debug, StructOpt)]
 /// Checks downloaded HereV1 maps and (optionally) deletes files that are corrupt so they can be downloaded again by the downloader.
-struct Opt {
+pub struct Opt {
     /// The directory where the downloaded maps are stored.
-    dir: PathBuf,
+    pub dir: PathBuf,
 
-    /// Do not delete corrupt files
+    /// Delete corrupt files without confirmation.
     #[structopt(short, long)]
-    keep_corrupt_files: bool,
+    pub force_delete: bool,
 }
 
-#[paw::main]
-fn main(opt: Opt) {
-    println!("{:?}", opt);
+quick_main!(run);
+
+fn run() -> Result<()> {
+    let bold = Style::new().bold();
+    let opt = Opt::from_args();
+    let path = opt.dir;
+    let update_file = {
+        let mut buf = path.clone();
+        buf.push("update.xml");
+        buf
+    };
+
+    println!("Using path: {}", bold.apply_to(path.to_string_lossy()));
+
+    let manifest: Manifest = with_message(
+        "reading update.xml",
+        || format!("Could not read: {:?}", update_file),
+        || {
+            let file = File::open(&update_file)?;
+            let parsed = from_reader(BufReader::new(file))?;
+            Ok(parsed)
+        },
+    )?;
+
+    let countries = manifest.countries()?;
+    let file_count = countries.iter().map(|c| c.file_count()).sum();
+
+    println!(
+        "Found maps for region: {} ({} countries in {} files)",
+        bold.apply_to(manifest.region_name()),
+        bold.apply_to(countries.len()),
+        bold.apply_to(file_count)
+    );
+
+    let zip_files: HashMap<_, _> = with_message(
+        "reading dir entries",
+        || format!("Could not read dir: {:?}", path),
+        || {
+            Ok(read_dir(&path)?
+                .filter_map(|f| match f {
+                    Err(e) => Some(Err(e.into())),
+                    Ok(e) if e.path().extension()? == "zip" => Some(Ok((
+                        e.path().file_name()?.to_string_lossy().into_owned(),
+                        e,
+                    ))),
+                    _ => None,
+                })
+                .collect::<Result<Vec<_>>>()?
+                .into_iter()
+                .collect())
+        },
+    )?;
+
+    println!(
+        "Found {} relevant files in path",
+        bold.apply_to(zip_files.len())
+    );
+
+    println!("Performing integrity check...");
+
+    let bars = Arc::new(MultiProgress::new());
+    let main_bar = bars.add(ProgressBar::new(file_count)).with_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40} file {pos:.bold} of {len:.bold} ({eta})"),
+    );
+    let bars_thread = {
+        let bars = bars.clone();
+        thread::spawn(move || {
+            bars.join_and_clear().unwrap();
+        })
+    };
+
+    let problems = Arc::new(Mutex::new(vec![]));
+    let sty =
+        ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} ({total_bytes:>8}) {wide_msg}");
+    countries
+        .par_iter()
+        .flat_map(|c| c.files())
+        .progress_with(main_bar)
+        .for_each_init(
+            || {
+                (
+                    problems.clone(),
+                    bars.add(ProgressBar::new(100)).with_style(sty.clone()),
+                )
+            },
+            |(problems, bar), file| match zip_files.get(&file.filename) {
+                None => problems.lock().unwrap().push(Problem::NotFound {
+                    filename: file.filename,
+                }),
+                Some(zip_file) => {
+                    let zip_size = zip_file.metadata().unwrap().len();
+                    let size = file.packedsize();
+                    if zip_size != size {
+                        problems.lock().unwrap().push(Problem::WrongSize {
+                            filename: file.filename,
+                            expected: size,
+                            got: zip_size,
+                        });
+                        return;
+                    }
+                    bar.set_message(&file.filename);
+                    bar.set_length(size);
+                    let expected = file.md5().to_string();
+                    match get_md5(bar, zip_file.path()) {
+                        Err(e) => problems.lock().unwrap().push(Problem::Error(e.into())),
+                        Ok(got) if got == expected => (),
+                        Ok(got) => problems.lock().unwrap().push(Problem::WrongSignature {
+                            filename: file.filename,
+                            got,
+                            expected,
+                        }),
+                    }
+                }
+            },
+        );
+
+    bars_thread.join().unwrap();
+    println!("Problems: {:#?}", problems.lock().unwrap());
+
+    println!("ZIP files: {}", zip_files.len());
+    Ok(())
+}
+
+fn with_message<R, EK: Into<ErrorKind>>(
+    msg: &str,
+    error: impl FnOnce() -> EK,
+    f: impl FnOnce() -> Result<R>,
+) -> Result<R> {
+    let pb = ProgressBar::new_spinner();
+    pb.set_message(&format!("{}", msg));
+    pb.enable_steady_tick(100);
+    f().chain_err(error)
+}
+
+fn get_md5(bar: &ProgressBar, path: impl AsRef<Path>) -> Result<String> {
+    let mut file = File::open(path)?;
+    let mut context = md5::Context::new();
+    let mut buffer = [0; 8 * 1024];
+    loop {
+        let n = file.read(&mut buffer)?;
+        if n == 0 {
+            break;
+        }
+        bar.inc(n as u64);
+        context.consume(&buffer[..n]);
+    }
+    Ok(format!("{:x}", context.compute()))
+}
+
+#[derive(Debug)]
+enum Problem {
+    NotFound {
+        filename: String,
+    },
+    WrongSize {
+        filename: String,
+        expected: u64,
+        got: u64,
+    },
+    WrongSignature {
+        filename: String,
+        expected: String,
+        got: String,
+    },
+    Error(errors::Error),
 }
---
+++ src/manifest.rs
@@ -0,0 +1,134 @@
+use crate::errors::*;
+use rayon::prelude::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+impl Manifest {
+    pub fn countries(&self) -> Result<Vec<&Country>> {
+        let mut country_map: HashMap<_, _> = self
+            .drm_entry
+            .map_catalog
+            .regions
+            .iter()
+            .flat_map(|r| &r.regions)
+            .map(|c| (c.id, c))
+            .collect();
+        self.drm_entry
+            .sales_region
+            .regions
+            .iter()
+            .map(|r| {
+                country_map
+                    .remove(&r.id)
+                    .chain_err(|| format!("Could not link country with id: {} in update.xml", r.id))
+            })
+            .collect()
+    }
+
+    pub fn region_name(&self) -> &str {
+        &self.drm_entry.sales_region.name
+    }
+}
+
+impl Country {
+    pub fn file_count(&self) -> u64 {
+        let opt_sr = if self.speech_recognition.is_some() {
+            1
+        } else {
+            0
+        };
+        self.data_groups.len() as u64 + opt_sr
+    }
+    pub fn files(&self) -> impl ParallelIterator<Item = ZipFile> {
+        self.data_groups
+            .par_iter()
+            .map(move |dg| ZipFile {
+                filename: format!("{}_{:02}.zip", self.id, dg.id),
+                info: &dg.info,
+            })
+            .chain(self.speech_recognition.as_ref().map(|info| ZipFile {
+                filename: format!("{}_speech_recognition.zip", self.id),
+                info,
+            }))
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Manifest {
+    drm_entry: DrmEntry,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct DrmEntry {
+    map_catalog: MapCatalog,
+    sales_region: SalesRegion,
+}
+
+#[derive(Debug, Deserialize)]
+struct MapCatalog {
+    #[serde(rename = "region")]
+    regions: Vec<Continent>,
+}
+
+#[derive(Debug, Deserialize)]
+struct SalesRegion {
+    name: String,
+
+    #[serde(rename = "region")]
+    regions: Vec<Region>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Region {
+    id: u32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Continent {
+    #[serde(rename = "region")]
+    regions: Vec<Country>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Country {
+    id: u32,
+    pub name: String,
+    #[serde(rename = "dataGroup")]
+    data_groups: Vec<DataGroup>,
+    speech_recognition: Option<FileInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FileInfo {
+    pub unpackedsize: String,
+    pub packedsize: String,
+    pub md5: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct DataGroup {
+    #[serde(flatten)]
+    info: FileInfo,
+    id: u32,
+}
+
+pub struct ZipFile<'a> {
+    info: &'a FileInfo,
+    pub filename: String,
+}
+
+impl<'a> ZipFile<'a> {
+    pub fn md5(&self) -> &str {
+        &self.info.md5
+    }
+
+    pub fn packedsize(&self) -> u64 {
+        self.info
+            .packedsize
+            .parse()
+            .expect("Could not parse packedsize")
+    }
+}
